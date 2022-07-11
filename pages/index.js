49972f2c8e6eb85517d38db351760b0225622789
Md Shahriyar Alam <contact@shahriyar.dev>
2022-07-11 23:34:35 +0600

    Add todo working

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,19 @@ import AddForm from '../components/AddForm'
 import Todos from '../components/Todos'
 
 const Home = () => {
-  const [todos, setTodos] = useState([
-    {_id: 1, content: "Hello world"},
-    {_id: 2, content: "This is second task"}
-  ])
+  const [todos, setTodos] = useState([])
 
-  const addTodo = (todoData, callback) => {
-    console.log(todoData)
+  const addTodo = async (content, callback) => {
+    const data = await fetch("http://localhost:3000/api/todo", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ content })
+    }).then(response => response.json())
+    
+    const newTodos = [data, ...todos]
+    setTodos(newTodos)
     callback()
   }
 
